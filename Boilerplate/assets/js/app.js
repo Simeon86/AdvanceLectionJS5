b40d429602a26c074a15e26554231a22c116dabc
Simeon86 <you@example.com>
2022-12-01 09:31:47 +0200

    first commit

---
+++ Boilerplate/assets/js/app.js
@@ -0,0 +1,102 @@
+var personArr = [{
+    name: 'Simeon',
+    lastName: 'Nikolov',
+    w: 103,
+    h: 189,
+    city: 'Varna',
+    phone: '35999999999',
+    address: 'Ave Str. 156',
+    job: {
+        title: 'IT Specialist',
+        company: 'Advance Academy'
+    },
+    sayHello: function() {
+        console.log('Hi I am ' + this.name + '. How are you?')
+    },
+    sayAddress: function() {
+        console.log('My address is ' + this.address + ', ' + this.city)
+    }
+}, {
+    name: 'Valeriya',
+    lastName: 'Nikolova',
+    w: 50,
+    h: 170,
+    city: 'Varna',
+    phone: '35999999999',
+    address: 'Ave Str. 156',
+    job: {
+        title: 'IT Specialist',
+        company: 'Advance Academy'
+    },
+    sayHello: function() {
+        console.log('Hi I am ' + this.name + '. How are you?')
+    },
+    sayAddress: function() {
+        console.log('My address is ' + this.address + ', ' + this.city)
+    }
+}, {
+    name: 'Kaloyan',
+    lastName: 'Nikolov',
+    w: 110,
+    h: 20,
+    city: 'Varna',
+    phone: '35999999999',
+    address: 'Ave Str. 156',
+    job: {
+        title: 'IT Specialist',
+        company: 'Advance Academy'
+    },
+    sayHello: function() {
+        console.log('Hi I am ' + this.name + '. How are you?')
+    },
+    sayAddress: function() {
+        console.log('My address is ' + this.address + ', ' + this.city)
+    }
+}, {
+    name: 'Nadejda',
+    lastName: 'Nikolova',
+    w: 50,
+    h: 165,
+    city: 'Varna',
+    phone: '35999999999',
+    address: 'Ave Str. 156',
+    job: {
+        title: 'IT Specialist',
+        company: 'Advance Academy'
+    },
+    sayHello: function() {
+        console.log('Hi I am ' + this.name + '. How are you?')
+    },
+    sayAddress: function() {
+        console.log('My address is ' + this.address + ', ' + this.city)
+    }
+}, {
+    name: 'John',
+    lastName: 'Doe',
+    w: 95,
+    h: 185,
+    city: 'New York',
+    phone: '35999999999',
+    address: 'Ave Str. 156',
+    job: {
+        title: 'IT Specialist',
+        company: 'Advance Academy'
+    },
+    sayHello: function() {
+        console.log('Hi I am ' + this.name + '. How are you?')
+    },
+    sayAddress: function() {
+        console.log('My address is ' + this.address + ', ' + this.city)
+    }
+}]
+
+personArr[0].sayHello()
+personArr[0].sayAddress()
+personArr[1].sayHello()
+personArr[1].sayAddress()
+personArr[2].sayHello()
+personArr[2].sayAddress()
+personArr[3].sayHello()
+personArr[3].sayAddress()
+personArr[4].sayHello()
+personArr[4].sayAddress()
